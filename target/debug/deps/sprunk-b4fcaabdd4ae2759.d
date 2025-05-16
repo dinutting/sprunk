bf86a591e35ab6989d8318ccf79bc3340c8fe999
dinutting <dinutting@outlook.com>
2025-05-15 22:53:34 -0400

    init

---
+++ target/debug/deps/sprunk-b4fcaabdd4ae2759.d
@@ -0,0 +1,5 @@
+/Users/danny/Documents/Programming/Rust/sprunk/target/debug/deps/libsprunk-b4fcaabdd4ae2759.rmeta: src/main.rs
+
+/Users/danny/Documents/Programming/Rust/sprunk/target/debug/deps/sprunk-b4fcaabdd4ae2759.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/sprunk-fdc17dc3f49c57b8.d
@@ -0,0 +1,5 @@
+/Users/danny/Documents/Programming/Rust/sprunk/target/debug/deps/libsprunk-fdc17dc3f49c57b8.rmeta: src/main.rs
+
+/Users/danny/Documents/Programming/Rust/sprunk/target/debug/deps/sprunk-fdc17dc3f49c57b8.d: src/main.rs
+
+src/main.rs:
